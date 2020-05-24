9a9716a22c02d4d9ed9a9dc9e274296bc196ef8a
ChenXbFrank <810464826@qq.com>
2020-05-24 09:18:37 +0800

    修改路由

--- src/main/java/com/cxb/springcloud/web/PayController.java
+++ src/main/java/com/cxb/springcloud/web/PayController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/pay")
 public class PayController {
 
     @GetMapping("/balance")
